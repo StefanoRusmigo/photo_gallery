61fca875482545601524387b3b88e34bed08abd8
Stefano Rusmigo <stefanorusmigo@hotmail.com>
2018-03-15 09:50:58 +0200

    Update config.php

--- private/includes/config.php
+++ private/includes/config.php
@@ -4,7 +4,7 @@
 
 defined('DB_SERVER')   ? null : define('DB_SERVER','localhost');
 defined('DB_USER')     ? null : define('DB_USER','gallery');
-defined('DB_PASSWORD') ? null : define('DB_PASSWORD','Play129081990!');
+defined('DB_PASSWORD') ? null : define('DB_PASSWORD','');
 defined('DB_NAME')     ? null : define('DB_NAME','photo_gallery');
 
 
